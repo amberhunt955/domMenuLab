81205f634760e087324e3adfd6f9df514ec585dc
Amber Hunt <amberhunt955@gmail.com>
2023-02-21 14:00:09 -0500

    completed html and css as per instructions

---
+++ index.js
@@ -0,0 +1,11 @@
+// In the Intro to the DOM we selected, manipulated and created 
+// DOM elements - this lab provides practice doing the same.
+
+// This is the first of a two-part lab that builds a 
+// menu bar with a slide-down submenu.
+
+// Note: Several of the tasks in this lab would be better done 
+// upfront in the markup or CSS instead of using JS, however the 
+// goal of this lab is to provide practice modifying the DOM using 
+// JS. In your projects, if the HTML or CSS is known in advance 
+// and/or static (unchanging), code it in HTML and CSS!
