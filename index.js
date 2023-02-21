47d6da5e559c7fbd4b0b50e9315e5504ff1b9457
Amber Hunt <amberhunt955@gmail.com>
2023-02-21 15:18:24 -0500

    completed week 5 day 1 homework

--- index.js
+++ index.js
@@ -9,3 +9,54 @@
 // goal of this lab is to provide practice modifying the DOM using 
 // JS. In your projects, if the HTML or CSS is known in advance 
 // and/or static (unchanging), code it in HTML and CSS!
+
+// Task 1.0 - Select and cache the <main> element in a variable named mainEl.
+const mainEl = document.querySelector("main");
+
+// Task 1.1 - Set the background color of mainEl to the value 
+// stored in the --main-bg CSS custom property. Hint: Assign a 
+// string that uses the CSS var()function like this: 'var(--main-bg)'
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+// Task 1.2 - Set the content of mainEl to <h1>SEI Rocks!</h1>.
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+
+// Task 1.3 - Add a class of flex-ctr to mainEl.
+mainEl.classList.add("flex-ctr");
+
+// Task 2.0 Select and cache the <nav id="top-menu"> 
+// element in a variable named topMenuEl.
+const topMenuEl = document.querySelector("nav");
+
+// Task 2.1 Set the height topMenuEl element to be 100%.
+topMenuEl.style.height = "100%";
+
+// Task 2.2 Set the background color of topMenuEl to the 
+// value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+// Task 2.3 Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add("flex-around");
+
+// Task 3.0 Copy the following data structure:
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+// Task 3.1 Iterate over the entire menuLinks array and for each "link" object:
+  // (1) Create an <a> element.
+  // (2) On the new element, add an href attribute with its value 
+      // set to the href property of the "link" object.
+  // (3) Set the new element's content to the value of the text property of the "link" object.
+  // (4) Append the new element to the topMenuEl element.
+
+  menuLinks.forEach( object => {
+    const a = document.createElement("a");
+    a.setAttribute("href", object.href); 
+    a.textContent = object.text;
+    topMenuEl.appendChild(a);
+} )
